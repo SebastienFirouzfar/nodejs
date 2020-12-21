a32a4db1a7fcb916754c991ed715f3ae4ce0a07e
Firouzfar <firouzfar.sebastien@gmail.com>
2020-12-21 16:55:02 +0100

    Mise en place la database

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
+//data base 
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
+
+//conntect to database
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Connected to db!");
+
+    app.listen(3000, () => console.log("Server Up and running"));
+});
+
 
 
 //accèdes au style css
 app.use("/static", express.static("public"));
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-// app.set("view eenginer", 'pug')
 
 //creation ficher ejs todo.ejs
 app.set("view engine", "ejs");
@@ -14,12 +30,21 @@ app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     //res.render() 	Génère un modèle de vue.
-    res.render('todo.ejs')
+    res.render("todo.ejs", { todoTasks: tasks });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+//POST METHOD
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
 
-// localhost:3000 server
-app.listen(3000, () => console.log("Serveur opérationnel"));
+
+
